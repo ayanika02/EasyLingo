462e5c75c2e8fbea40ce0100b8c9ae391bd0fc94
ayanika02 <66836997+ayanika02@users.noreply.github.com>
2023-10-20 12:48:47 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,8 +1,6 @@
 <?php
-//This script will handle login
 session_start();
 
-// check if the user is already logged in
 if(isset($_SESSION['username']))
 {
     header("location: welcome.php");
@@ -13,7 +11,6 @@ require_once "config.php";
 $username = $password = "";
 $err = "";
 
-// if request method is post
 if ($_SERVER['REQUEST_METHOD'] == "POST"){
     if(empty(trim($_POST['username'])) || empty(trim($_POST['password'])))
     {
@@ -32,8 +29,6 @@ if(empty($err))
     mysqli_stmt_bind_param($stmt, "s", $param_username);
     $param_username = $username;
     
-    
-    // Try to execute this statement
     if(mysqli_stmt_execute($stmt)){
         mysqli_stmt_store_result($stmt);
         if(mysqli_stmt_num_rows($stmt) == 1)
@@ -43,14 +38,11 @@ if(empty($err))
                     {
                         if(password_verify($password, $hashed_password))
                         {
-                            // this means the password is corrct. Allow user to login
                             session_start();
                             $_SESSION["username"] = $username;
                             $_SESSION["id"] = $id;
                             $_SESSION["loggedin"] = true;
-
-                            //Redirect user to welcome page
-                            header("location: welcome.php");
+                             header("location: welcome.php");
                             
                         }
                     }
@@ -69,17 +61,15 @@ if(empty($err))
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <style>
         body {
             background-image: url('bg.avif');
-            background-size: cover; /* Adjust this property to control how the image is displayed */
+            background-size: cover; 
             background-repeat: no-repeat;
         }
     </style>
-    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>EasyLingo</title>
@@ -131,8 +121,6 @@ if(empty($err))
 
 </div>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
