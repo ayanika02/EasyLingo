c218d2b9caeecfad06c641133827092b993fd0ca
ayanika02 <66836997+ayanika02@users.noreply.github.com>
2023-10-20 12:53:33 +0530

    Update register.php

--- register.php
+++ register.php
@@ -6,7 +6,6 @@ $username_err = $password_err = $confirm_password_err = "";
 
 if ($_SERVER['REQUEST_METHOD'] == "POST"){
 
-    // Check if username is empty
     if(empty(trim($_POST["username"]))){
         $username_err = "Username cannot be blank";
     }
@@ -16,11 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST"){
         if($stmt)
         {
             mysqli_stmt_bind_param($stmt, "s", $param_username);
-
-            // Set the value of param username
             $param_username = trim($_POST['username']);
-
-            // Try to execute this statement
             if(mysqli_stmt_execute($stmt)){
                 mysqli_stmt_store_result($stmt);
                 if(mysqli_stmt_num_rows($stmt) == 1)
@@ -39,8 +34,6 @@ if ($_SERVER['REQUEST_METHOD'] == "POST"){
 
     mysqli_stmt_close($stmt);
 
-
-// Check for password
 if(empty(trim($_POST['password']))){
     $password_err = "Password cannot be blank";
 }
@@ -50,14 +43,10 @@ elseif(strlen(trim($_POST['password'])) < 5){
 else{
     $password = trim($_POST['password']);
 }
-
-// Check for confirm password field
 if(trim($_POST['password']) !=  trim($_POST['confirm_password'])){
     $password_err = "Passwords should match";
 }
 
-
-// If there were no errors, go ahead and insert into the database
 if(empty($username_err) && empty($password_err) && empty($confirm_password_err))
 {
   $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
@@ -65,12 +54,9 @@ if(empty($username_err) && empty($password_err) && empty($confirm_password_err))
     if ($stmt)
     {
         mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
-
-        // Set these parameters
         $param_username = $username;
         $param_password = password_hash($password, PASSWORD_DEFAULT);
 
-        // Try to execute the query
         if (mysqli_stmt_execute($stmt))
         {
             header("location:login.php");
@@ -92,18 +78,16 @@ mysqli_close($conn);
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <style>
         body {
             background-image: url('bg.avif');
-            background-size: cover; /* Adjust this property to control how the image is displayed */
+            background-size: cover; 
             background-repeat: no-repeat;
         }
     </style>
 
-    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>EasyLingo</title>
