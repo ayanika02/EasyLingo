25e5cd665b5d6f8ae0cebd198e17495374c12e4f
ayanika02 <66836997+ayanika02@users.noreply.github.com>
2023-10-20 12:22:24 +0530

    Update config.php

--- config.php
+++ config.php
@@ -4,10 +4,8 @@ $username = "root";
 $password = "";
 $database = "login";
 
-// Create a connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 
-// Check the connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
@@ -19,10 +17,7 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'login');
 
-// Try connecting to the Database
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-//Check the connection
 if($conn == false){
     dir('Error: Cannot connect');
 }
